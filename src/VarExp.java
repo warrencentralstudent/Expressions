29f4a9dfc1a39dbd72a1f39fef22e77c38659b49
Davian Affognon <affogdav000@warren.k12.in.us>
2024-08-06 11:50:24 -0400

    done

--- src/VarExp.java
+++ src/VarExp.java
@@ -1,2 +1,11 @@
-public class VarExp {
+public class VarExpr {
+    public static void main(String[] args) {
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
+        myFirstNumber = 6;
+        System.out.println(myFirstNumber);
+        myFirstNumber = (10 + 5) + (2 * 10);
+        int myfirstnumber = 7;
+
+    }
 }
